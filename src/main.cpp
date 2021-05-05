060baa82aa892ab975ed25df54b4353d7b6f00a3
elsueter <joeojejoe123@hotmail.com>
2021-05-06 00:54:34 +0100

    Revert "Merge branch 'master' of https://github.com/elsueter/boolNet"
    This reverts commit 72bea81d0bc3aab391640d4f547e406dfce32265, reversing
    changes made to 0f56a710800ee3ece44441280175c9d95a358b6a.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "../include/boolnet.hpp"
+
+//Temperary includes
+#include "terminal/output.hpp"
+#include "network.cpp"
+
+int main(){
+
+    basicNetwork netA(BTT);
+
+    return 0;
+}
---
+++ src/terminal/output.hpp
@@ -0,0 +1,13 @@
+#ifndef _output_H_
+#define _output_H_
+#include <iostream>
+
+template<typename T>
+void printVector(std::vector<T> in);
+template<typename T>
+void printVector(std::vector<std::vector<T>> in);
+template<typename T>
+void printVector(std::vector<std::vector<std::vector<T>>> in);
+#include "vectorOut.cpp"
+
+#endif
