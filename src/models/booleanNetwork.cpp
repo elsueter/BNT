509f24793b43f7194e394a2d18abfb183971e28c
elsueter <joeojejoe123@hotmail.com>
2021-05-07 05:34:08 +0100

    state comparitor & trace threading start

--- src/models/booleanNetwork.cpp
+++ src/models/booleanNetwork.cpp
@@ -4,17 +4,58 @@ bool &state::operator[] (int index){
     return vals[index];
 }
 
+bool state::operator ==(state in){
+    if(in.len != len){
+        return false;
+    }
+    for(int i = 0; i < len; i++){
+        if(in[i] != vals[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 statePair &stateTable::operator [](int index){
     return table[index];
+}
+
+sequence::~sequence(){
+    for(int i = 0; i < len; i++){
+        delete states[i].vals;
+    }
+    delete states;
+}
+
+bool sequence::contains(state in){
+    for(int i = 0; i < len; i++){
+        if(in == states[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
+void sequence::push_back(state in){
+    state temp[len+1];
+    for(int i = 0; i < len; i++){
+        temp[i] = states[i];
+    }
+    temp[len] = in;
+    states = temp;
 }
 
 state &sequence::operator [](int index){
     return states[index];
 }
 
+sequence &sequenceArr::operator [](int index){
+    return sequences[index];
+}
+
 // booleanNetwork ------------------------------------------------------------------
 
-// Constructors
+// Constructors / Deconstructor
 
 booleanNetwork::booleanNetwork(std::vector<std::vector<int>> inTT){
     int len = inTT.size()/2;
@@ -31,28 +72,84 @@ booleanNetwork::booleanNetwork(std::vector<std::vector<int>> inTT){
             TT[i].t1[j] = inTT[(i*2)+1][j];
         }
     }
+
+    attractors = {new sequence[0], 0};
+    traces = {new sequence[0], 0};
+    uniqueTraces = {new sequence[0], 0};
 }
 
 booleanNetwork::booleanNetwork(stateTable inTT){
     TT = inTT;
 }
 
+booleanNetwork::~booleanNetwork(){
+    // TT
+    for(int i = 0; i < TT.len; i++){
+        delete TT[i].t0.vals;
+        delete TT[i].t1.vals;
+    }
+    delete TT.table;
+    
+    // Attractors
+    /*for(int i = 0; i < attractors.len; i++){
+        delete attractors[i].vals;
+    }*/
+    //delete attractors.sequences;
+
+    // Traces
+    /*for(int i = 0; i < traces.len; i++){
+        delete traces[i].vals;
+    }*/
+    //delete traces.sequences;
+    
+    // Unique Traces
+    /*for(int i = 0; i < uniqueTraces.len; i++){
+        delete uniqueTraces[i].vals;
+    }*/
+    //delete uniqueTraces.sequences;
+}
+
 // Network Functions
 
 void booleanNetwork::genTraces(){
+    //std::thread threads[TT.len];
     for(int i = 0; i < TT.len; i++){
         traces.len++;
-        traces.states = new state[traces.len];
-        std::thread temp(genTrace, TT[i].t0);
+        traces.sequences = new sequence[traces.len];
+        traces[i].states = new state[0];
+        traces[i].states[0] = TT[i].t0;
+        genTrace(traces[i]);
+        //threads[i] = std::thread(&booleanNetwork::genTrace, this, std::ref(traces[i]));
     }
+    
+    //for(int i = 0; i < TT.len; i++){
+        //threads[i].join();
+        //threads[i].~thread();
+    //}
 }
-        
-void booleanNetwork::genTrace(state in){
 
+void booleanNetwork::genTrace(sequence &trace){
+    bool attractorHit = false;
+    //while(!attractorHit){
+        for(int i = 0; i < TT.len; i++){
+            if(TT[i].t0 == trace[trace.len-1]){
+                if(trace.contains(TT[i].t1)){
+                    attractorHit = true;
+                    break;
+                }else{
+                    trace.push_back(TT[i].t1);
+                }
+            }
+        }
+    //}
 }
 
 // variable exposure functions
 
 stateTable booleanNetwork::getTT(){
     return TT;
+}
+
+sequenceArr booleanNetwork::getTraces(){
+    return traces;
 }
