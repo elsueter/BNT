6d5531060ca538b945b912d951d1b610f4d86cbb
elsueter <34282243+elsueter@users.noreply.github.com>
2021-05-30 15:51:43 +0100

    Comments

--- include/BNT/booleanNetwork.h
+++ include/BNT/booleanNetwork.h
@@ -6,15 +6,18 @@
 
 #include <crow/json.h>
 #include <vector>
-#include <thread>
+//#include <thread>
 #include <string>
-#include <fstream>
+#include <fstream> //File operations (read/write)
 
 // Network Structs ----------------------------------------------------------------------------
 
 namespace BooleanNetwork{
 
+//"State" is an array of bools (ints for speed & typedef vector acting as a "stuct") that describe a networks state
+//eg: {0, 0, 1} describes a 3 node network with A=0, B=0, C=1
 typedef std::vector<int> state;
+//a sequence of states that may hold traces and attractors of a network
 typedef std::vector<state> sequence;
 
 struct nodeMap{
@@ -22,32 +25,43 @@ struct nodeMap{
 	bool nodeValue;
 };
 
+//Maps an input state of nodes to this nodes resultant state
 struct statePair{
+	//input state
 	std::vector<nodeMap> in;
+	//output value
 	bool out;
 
 	bool operator ==(state);
 };
 
+//Node describes the behaviour for a node in a network.
 struct node {
+	//Array of statePairs acting as a local truth table.
 	std::vector<statePair> TT;
+	//The input expression that defines the behaviour and therefore truth table.
 	std::string exp;
+	//Current value of the node and the next value buffered for iteration.
 	bool curr;
 	bool next;
 
+	//Check the input state against the truth table then buffer the future value for the node
 	void check(state);
+	//Iterate the node to its future state
 	void update();
 };
 
+//Defines the structure for a full network (array of nodes)
 typedef std::vector<node> netStruc;
 typedef std::vector<netStruc> netStrucArr;
 
 // Output Utilities--------------------------------------------------------------------------------------
 
-std::string stringify(netStruc in);
+//std::string stringify(netStruc in);
 
 // Network Utilities--------------------------------------------------------------------------------
 
+//Generalised function for checking if array contains member
 template<typename T>
 bool vectorContains(std::vector<T> arr, T in){
     for(auto& it: arr){
@@ -58,37 +72,50 @@ bool vectorContains(std::vector<T> arr, T in){
     return false;
 }
 
+//Functions for parsing a JSON file containing descriptors of networks, returning passible network structures
 netStrucArr parseFile();
 netStrucArr parseFile(std::string path);
 
+//Parses an input expression string and returns a network structure
 netStruc parseExpression(std::vector<std::string> in);
 
 // Network Classes--------------------------------------------------------------------------------------
 
+//Node based network model
 class nodeNetwork{
-	private:
-		netStruc nodes;
+private:
+	//Structure of the network
+	netStruc nodes;
 
-		std::vector<sequence> traces;
-		std::vector<sequence> attractors;
-		std::vector<sequence> uniqueTraces;
+	sequence trace;
+	std::vector<sequence> attractors;
+	std::vector<sequence> uniqueTraces;
 
-		void iterateAll(state in);
+	//Iterates every node from a given state
+	void iterateAll(state in);
 
-	public:
-		nodeNetwork(netStruc in);
+public:
+	//Constructor
+	nodeNetwork(netStruc in);
 
-		void setState(state in);
-		void synchronusIterate();
-		void synchronusIterate(state start);
-		void synchronusIterate(crow::json::rvalue start);
+	//Sets the current state of the network
+	void setState(state in);
 
-		void clear();
+	//Iterates every node synchonusly until an attractor is hit (from current or given state)
+	void synchronusIterate();
+	void synchronusIterate(state start);
+	void synchronusIterate(crow::json::rvalue start);
 
-		state getState();
-		std::string getTraces();
-		std::vector<sequence> getAttractors();
-		std::vector<sequence> getUniqueTraces();
+	//Clears the calculated structures (traces/attractors/etc...)
+	void clear();
+
+	//Returns the current state of the network
+	state getState();
+
+	//Private variable get methods
+	std::string getTrace();
+	std::vector<sequence> getAttractors();
+	std::vector<sequence> getUniqueTraces();
 };
 
 }
--- include/BNT/src/nodeNetwork.cpp
+++ include/BNT/src/nodeNetwork.cpp
@@ -3,6 +3,7 @@
 using namespace BooleanNetwork;
 
 void nodeNetwork::iterateAll(state in){
+    //Iterates over every node in the network, generated the future state and steps forwards
     for(auto& it: nodes){
         it.check(in);
         it.update();
@@ -20,17 +21,16 @@ void nodeNetwork::setState(state start){
 }
 
 void nodeNetwork::synchronusIterate(){
-    sequence temp;
-    temp.push_back(getState());
+    trace.clear();
+    trace.push_back(getState());
     bool attractorHit = false;
     while(!attractorHit){
         iterateAll(getState());
-        if(vectorContains(temp, getState())){
+        if(vectorContains(trace, getState())){
             attractorHit = true;
         }
-        temp.push_back(getState());
+        trace.push_back(getState());
     }
-    traces.push_back(temp);
 }
 
 void nodeNetwork::synchronusIterate(state start){
@@ -38,18 +38,17 @@ void nodeNetwork::synchronusIterate(state start){
     synchronusIterate();
 }
 
-void nodeNetwork::synchronusIterate(crow::json::rvalue start){
-    state temp;
-    temp.reserve(start.size());
-    for(auto& it: start){
-        temp.push_back(it.i());
+void nodeNetwork::synchronusIterate(crow::json::rvalue startJSON){
+    state start;
+    start.reserve(startJSON.size());
+    for(auto& it: startJSON){
+        start.push_back(it.i());
     }
-    setState(temp);
-    synchronusIterate();
+    synchronusIterate(start);
 }
 
 void nodeNetwork::clear(){
-    traces.clear();
+    trace.clear();
     attractors.clear();
     uniqueTraces.clear();
 }
@@ -62,21 +61,19 @@ state nodeNetwork::getState(){
     return out;
 }
 
-std::string nodeNetwork::getTraces(){
+std::string nodeNetwork::getTrace(){
     std::string out = "dinetwork {node[shape=circle];";
-    for(auto& trace: traces){
-        for(int i = 0; i < trace.size(); i++){
-            out += '"';
-            for(auto& num: trace[i]){
-                out += std::to_string(num);
-            }
-            out += '"';
-            if(i != trace.size()-1){
-                out += "->";
-            }
+    for(int i = 0; i < trace.size(); i++){
+        out += '"';
+        for(auto& num: trace[i]){
+            out += std::to_string(num);
         }
-        out += ';';
+        out += '"';
+        if(i != trace.size()-1){
+            out += "->";
+        }
     }
+    out += ';';
     out += "}";
     return out;
 }
